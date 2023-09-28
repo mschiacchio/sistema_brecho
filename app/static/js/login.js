f6ae5725500df8b1e6002d944731297d39cf96d4
mschiacchio <marcelo_chiacchio@hotmail.com>
2023-09-27 21:36:57 -0300

    Tela de login concluída

---
+++ app/static/js/login.js
@@ -0,0 +1,12 @@
+function showPassword(){
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('show-password')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    }else{
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+    }
+}
